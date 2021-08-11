3519b476d09dec27d58d31b68f93baa0bd132815
DESKTOP-7NKK714\AMD <tndus5837@gmail.com>
2021-08-11 15:29:02 +0900

    copyRight component

---
+++ src/components/common/CopyRight.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CopyRight: React.FC = () => {
+  return (
+    <Text>
+      © 2021 GwakSuyeon.{'\n'}
+      ALL RIGHTS RESERVED.
+    </Text>
+  );
+};
+
+const Text = styled.p`
+  position: fixed;
+  left: 2%;
+  bottom: 2%;
+  font-weight: 300;
+  color: #52575d;
+  white-space: pre-line;
+`;
+
+export default CopyRight;
