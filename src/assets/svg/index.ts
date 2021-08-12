dfaa7875b63a7d74ca617805793263e34d148947
DESKTOP-7NKK714\AMD <tndus5837@gmail.com>
2021-08-12 18:29:52 +0900

    add svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,15 @@
+export {default as SvgHtml5} from './skill/html5';
+export {default as SvgCss3} from './skill/css3';
+export {default as SvgJavascript} from './skill/javascript';
+export {default as SvgTypescript} from './skill/typescript';
+export {default as SvgReact} from './skill/react';
+export {default as SvgRedux} from './skill/redux';
+export {default as SvgReduxSaga} from './skill/reduxSaga';
+export {default as SvgStyledComponent} from './skill/styledComponent';
+export {default as SvgScss} from './skill/scss';
+
+export {default as SvgGit} from './communication/git';
+export {default as SvgNotion} from './communication/notion';
+export {default as SvgSlack} from './communication/slack';
+export {default as SvgTrello} from './communication/trello';
+export {default as SvgZeplin} from './communication/zeplin';
